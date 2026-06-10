482ac9250a4d5495e11762a72d3c4d7044f4cb0d
TheRedKardinal <claudio_giasi@icloud.com>
2026-06-10 17:40:54 +0200

    Esercizio finito e variabili spostate in file partial

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.getElementById('toggle-tema');
+
+toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    toggleBtn.textContent = document.body.classList.contains('dark') ? 'Tema chiaro' : 'Tema scuro';
+});
